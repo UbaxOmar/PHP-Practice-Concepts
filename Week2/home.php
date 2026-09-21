a2f0e5187bfc8a256f62aefda1f314482d1f2d40
UbaxOmar <UbaxOmar4@gmail.com>
2026-09-21 18:47:12 +0300

    update week 2 with php arrays practice

--- Week2/home.php
+++ Week2/home.php
@@ -7,76 +7,56 @@
 </head>
 <body>
    <?php
+   
+   // creating array - numerically
+   $names = array();
 
-// 1. CONSTANTS EXAMPLE
-// Defining a constant using the define() function
-// Notice: No $ sign is used for constant names, and UPPERCASE is recommended.
-define("PI", 3.14);
+   //second way to intilialize array
+   $names  [0] ="upah im programmer";
+    $names  [1] =123;
+    $names[]=12.34;
 
-echo "Value of PI is: ", PI, "<br><br>"; // Passing multiple parameters to echo
+    echo $names[0] . "<br>";
+    echo $names[1] . "<br>";
+     echo $names[2] . "<br>";
 
-// 2. IF-ELSEIF-ELSE STATEMENT
+    // display all
+    echo "<br>";
+    echo "<pre>";
+    var_dump($names);
+    echo "</pre>";
 
-$marks = 87;
+    $info = array(
+        "101", 
+        "Upah omar alasow",
+        20,
+        "Karan District",
+        "single"
+    );
 
-echo "<h3>Grade Evaluation (if-elseif):</h3>";
+    //To loop through and print all the values of an indexed array, you could use for lopp
+        echo"array values using for loop: <br>";
+        for($i =0; $i <count($info); $i++){
+            echo $info [$i] . "<br>";
+        
+        }
 
-if ($marks >= 90) {
-    echo "Excellent<br><br>";
-} elseif ($marks >= 80) {
-    echo "Very good<br><br>";
-} elseif ($marks >= 50) {
-    echo "Minimal pass<br><br>";
-} else {
-    echo "Fail<br><br>";
-}
+        //example of associative arrray to store information about a person
 
-// 3. SWITCH CASE STATEMENT
+        $info = array (
+            "id"=>"101",
+            "name"=>"upah omar",
+            "age"=>20,
+            "address"=>"karan District",
+            "status"=>"single",
+            "weight"=> 160.5
+        );
+        echo "<pre>";
+        echo "information about rhe person: <br>";
+        print_r($info);
+        var_dump($info);
+        echo"</pre>";
 
-echo "<h3>Grade Evaluation (switch-case):</h3>";
-
-switch (true) {
-    case ($marks >= 90):
-        echo "Grade: Excellent<br><br>";
-        break;
-    case ($marks >= 80):
-        echo "Grade: Very Good<br><br>";
-        break;
-    case ($marks >= 50):
-        echo "Grade: Minimal Pass<br><br>";
-        break;
-    default:
-        echo "Grade: Fail<br><br>";
-        break;
-}
-
-// 4. FOR LOOPS EXAMPLES
-
-echo "<h3>Multiplication Table (12 Times Table):</h3>";
-
-for ($count = 1; $count <= 12; ++$count) {
-    echo "$count times 12 is " . ($count * 12) . "<br>";
-}
-
-echo "<h3>Counting from 1 to 15:</h3>";
-
-for ($count = 1; $count <= 15; $count++) {
-    echo "$count <br>";
-}
-for ($i = 1; $i <= 5; $i++) {
-    for ($j = 1; $j <= 5; $j++) {
-        echo "$i * $j = " . ($i * $j) . "<br>";
-    }
-}
-
-echo "<br><hr><br>";
-
-// HABKA 2: Row is 1, Column is 1, Result is 1 Format
-for ($i = 1; $i <= 5; $i++) {
-    for ($j = 1; $j <= 5; $j++) {
-        echo "Row is $i, Column is $j, Result is " . ($i * $j) . "<br>";
-    }
-}
 ?>
 </body>
 </html>
